e95199237e6d0351a556ad48c2963d313e26443c
Luis Benavides Andrade <luis.benavides.andrade@gmail.com>
2021-09-16 12:51:09 +0200

    DSSi comment: update dssi_srv_scraper_stockmarket view add matplotlib library

--- dssi_srv_scraper_stockmarket/views.py
+++ dssi_srv_scraper_stockmarket/views.py
@@ -1,9 +1,14 @@
 """DSSi comment: standar package """
 from django.shortcuts import render
+from django.http import HttpResponse
+from matplotlib.figure import Figure
 import requests #https://pypi.org/project/requests/
 from bs4 import BeautifulSoup #https://pypi.org/project/beautifulsoup4/
 import numpy as np
-import pandas as pd
+import pandas as pd #https://pypi.org/project/pandas/
+import matplotlib.pyplot as plt #https://pypi.org/project/matplotlib/
+import base64
+from io import BytesIO
 
 """DSSi comment: class message log"""
 class Cl_message:
@@ -36,6 +41,7 @@ def srv_scraper_stockmarket(request):
         vr_stockvaluelist   = []
         vr_stocknamelist    = []
         df_stockmarkettop5  = pd.DataFrame()
+        df_stockmarkettop5_ind_html  = pd.DataFrame()
 
         for vr_web_url in vr_web_urls_source:
 
@@ -64,25 +70,39 @@ def srv_scraper_stockmarket(request):
                 vr_message_out = vr_message.mt_message_out_error('srv_scraper_stockmarket <strong>requests.get</strong> url source', f'{vr_web_url}')
                 vr_message_out += vr_message.mt_message_out_error('srv_scraper_stockmarket <strong>requests.get</strong> status_code', f'{vr_requests.status_code}')
 
+        #build a dataframe with the name and value of the stock
+        vr_stockvaluelist = list(map(lambda elem: elem.replace(',', ''), vr_stockvaluelist))
+        vr_stockvaluelist = list(map(float, vr_stockvaluelist))
+        vr_message.mt_message_log('srv_scraper_stockmarket vr_stockvaluelist',vr_stockvaluelist)
         vr_dict_of_lists = {'Stock':vr_stocknamelist, 'Value':vr_stockvaluelist}
         df_stockmarkettop5 = pd.DataFrame(vr_dict_of_lists)
         df_stockmarkettop5_ind = df_stockmarkettop5.set_index("Stock")
-        vr_message.mt_message_log('srv_scraper_stockmarket df_stockmarkettop5',df_stockmarkettop5_ind)
+        vr_message.mt_message_log('srv_scraper_stockmarket df_stockmarkettop5_ind',df_stockmarkettop5_ind)
         df_stockmarkettop5_ind_html = df_stockmarkettop5_ind.to_html()
-        
+
+        #build a plt bar to corparer of each other
+        fig = plt.Figure()    
+        ax = fig.subplots()
+        ax.bar(df_stockmarkettop5_ind.index, df_stockmarkettop5_ind["Value"])
+        ax.set_xticklabels(df_stockmarkettop5_ind.index, rotation=45)
+        buf = BytesIO()
+        fig.set_size_inches(8, 4)
+        fig.savefig(buf, format="png")
+        vr_stockmarket_graph = base64.b64encode(buf.getbuffer()).decode("ascii")
 
     except Exception as msg_error:
         vr_message.mt_message_log_error('srv_scraper_stockmarket',f'{msg_error}')
         vr_message_out = vr_message.mt_message_out_error('srv_scraper_stockmarket',f'{msg_error}...')
 
     context = {
-        'title':'DSSi - Stock Market Top 5 Scraper',
+        'title':'<span class="btn dssi-style">DSSi App</span> - Stock Market Top 5 Scraper',
         'vr_message_out':vr_message_out,
         'vr_web_urls_source':vr_web_urls_source,
         'vr_soupb':f'DSSi msg vr_soupb span_tag_attr string {vr_listoftargetvalues[:450]}...',
         'vr_stocknamelist':vr_stocknamelist,
         'vr_stockvaluelist':vr_stockvaluelist,
-        'df_stockmarkettop5':df_stockmarkettop5_ind_html    
+        'df_stockmarkettop5':df_stockmarkettop5_ind_html,    
+        'vr_stockmarket_graph':vr_stockmarket_graph
     }
 
     return render(request,'srv_scraper_stockmarket/srv_scraper_stockmarket.html', context)
