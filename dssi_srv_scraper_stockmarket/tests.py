1c538fb7fac339b1e6779815e20f0a320f95f8f5
Luis Benavides Andrade <luis.benavides.andrade@gmail.com>
2021-09-20 17:58:35 +0200

    DSSi comment: create unit test dssi_srv_scraper_stockmarket

--- dssi_srv_scraper_stockmarket/tests.py
+++ dssi_srv_scraper_stockmarket/tests.py
@@ -1,3 +1,15 @@
+"""DSSi comment: standard package """
 from django.test import TestCase
+import requests
 
 # Create your tests here.
+class srv_scraper_stockmarket(TestCase):
+
+    def test_check_request_status_ok(self):
+        self.vr_requests = requests.get('https://www.investing.com/equities/microsoft-corp')
+        print(f"test_check_request_status_ok {self.assertEqual(self.vr_requests.status_code, 200)}")
+
+    def test_check_request_status_ko(self):
+        self.vr_requests = requests.get('https://www.investing.com/equities/microsoft-corp123')
+        print(f"test_check_request_status_ko {self.assertEqual(self.vr_requests.status_code, 404)}")
+        
